1b350643309934c10901c3255b5e9e3f75087d4f
Kaspar <kaspar.tullus@gmail.com>
2022-12-01 02:54:34 +0200

    culture.js,culture.html changes. Made my search work with diffrent culture filters.

--- js/culture.js
+++ js/culture.js
@@ -1,69 +1,109 @@
+//Kaspar Tullus
 import SearchData from '../api/myHelsinkiApiNew.js';
 
 const mainElem = document.getElementById("cultureMain");
 const headerElem = document.getElementById('topHeader');
 const searchButton = document.getElementById('hakunappi');
 const searchInputField = document.getElementById('hakuteksti');
-const apiUrlSearchTab = "v1/events/?tags_filter=culture,";
+const apiUrlSearchTab = "v1/events/?tags_search=";
 
 let currentSearch;
 let keyword;
-let lastsearch = "";
-const datalist = [{
-    name:""
+let tagi = 0;
+const datalist = [{}];
+const tags = [{
+    name: "Musiikki,"
+},{
+    name: "Libraries,"
+},{
+    name: "Elokuvat,"
 }];
 function findCultureData(){
+    if (tagi == tags.length){
+        console.log("DONE")
+        tagi = 0;
+    }
     //Luetaan käyttäjänsyöte
     keyword = searchInputField.value;
     //Luodaan hakuolio
     currentSearch = new SearchData();
     //Tehdään haku
-    currentSearch.doQuery(apiUrlSearchTab,keyword);
+    currentSearch.doQuery(apiUrlSearchTab + tags[tagi].name, keyword);
+    LaunchCultureData();
 
+}
+function LaunchCultureData(){
+    console.log("CULTUREDATA" + tagi)
     setTimeout(function() {
-        for (let i = 0; i < currentSearch.resultJson.length; i++){
+        for (let i = 0; i < currentSearch.resultJson.length; i++) {
 
             console.log(currentSearch.resultJson[i].name.fi);
-            datalist.push(currentSearch.resultJson[i].name.fi);
+            datalist.push({
+                name: currentSearch.resultJson[i].name.fi,
+                description: currentSearch.resultJson[i].description.intro,
+                id: currentSearch.resultJson[i].id,
+                bodydes: currentSearch.resultJson[i].description.body
+            });
 
-            if (currentSearch.resultJson[i].name.fi === datalist[i]){
+            if (currentSearch.resultJson[i].name.fi === datalist[i].name &&
+                currentSearch.resultJson[i].description.intro == datalist[i].description ||
+                currentSearch.resultJson[i].id == datalist[i].id ||
+                currentSearch.resultJson[i].description.body == datalist[i].bodydes)
+            {
                 console.log("duplicate")
-
+            }else if (currentSearch.resultJson[i].description.images[0] == null && currentSearch.resultJson[i].description.intro == null || currentSearch.resultJson[i].description.body == null){
+                console.log("no info,not adding")
             }else {
 
+                let articletwo = document.createElement("article");
+                articletwo.className = "CULTDATARTCLASStwo";
+
                 let article = document.createElement("article");
+                article.className = "CULTDATARTCLASS";
                 article.id = "DataARTICLE" + i;
-                article.style = " margin: 2%;" +
-                    "padding: 0% 1% 0%;" +
-                    "display: flex;";
+                article.classList.add('CULTDATARTCLASS','new-CULTDATARTCLASS');
 
                 let img = document.createElement("img");
-                img.style = "height:25%; width:25%;";
+                img.className = "CultIMG";
 
-                if (currentSearch.resultJson[i].description.images[0].url == null) {
-                    img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png";
-                }else if (currentSearch.resultJson[i].description.images[0].url != null){
+
+                if (currentSearch.resultJson[i].description.images[0] == null) {
+                    img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png";
+                }else if (currentSearch.resultJson[i].description.images[0] != null){
                     img.src = currentSearch.resultJson[i].description.images[0].url;
                 }
 
-                let h2 = document.createElement("h2");
-                h2.innerText = currentSearch.resultJson[i].name.fi;
-                h2.class = "DataHEADER";
-                h2.style = " margin: 1%;" +
+                let h3 = document.createElement("h3");
+                h3.innerText = currentSearch.resultJson[i].name.fi;
+                h3.className = "DataHEADER";
+                h3.style = " margin: 1%;" +
                     "padding: 0;" +
-                    "width: 25%;";
+                    "width: 100%;";
+
                 let p = document.createElement("p");
-                p.class = "DataPARAGRAPH";
-                p.innerText = currentSearch.resultJson[i].description.intro
+                p.className = "DataPARAGRAPH";
+                p.innerHTML = currentSearch.resultJson[i].description.body
                 let br = document.createElement("br");
 
+                mainElem.appendChild(br)
+                mainElem.appendChild(articletwo)
+                articletwo.appendChild(h3);
                 mainElem.appendChild(article);
                 article.appendChild(img);
-                article.appendChild(h2);
                 article.appendChild(p);
             }
         }
-    }, 500);
+
+    }, 200);
+    tagi++
+    if (tagi <= tags.length - 1){
+        setTimeout(function () {
+            findCultureData()
+        },1000);
+    }else{
+
+        console.log("DONE")
+    }
 }
 
 
