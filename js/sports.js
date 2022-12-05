e429699819237eb170ad76ebdb37feadf584c20c
Samu <65758544+SmuuSka@users.noreply.github.com>
2022-12-05 22:39:28 +0200

    popup works

--- js/sports.js
+++ js/sports.js
@@ -7,11 +7,18 @@ const searchBox = document.getElementById('searchBox');
 const searchBtn = document.getElementById('searchBtn');
 const divElem = document.getElementById('result');
 
+//popup
+const popup = document.getElementById('popup');
+const closePopupBtn = document.getElementById('closePopup');
+let popupHeader = document.getElementById('popupHeader');
+let popupDescription = document.getElementById('popupDesc');
+
+//newDict
+let newDict = [];
 
 const apiUrlSearchTab = "v1/events/?tags_filter=sports";
 
 const map = document.getElementById('map');
-
 
 let currentSearch;
 let keyword;
@@ -20,11 +27,15 @@ let waitTime = 0;
 let events;
 let currentDay;
 
+let article;
+
 
 window.addEventListener("load", () => {
     console.log("This function is executed once the page is fully loaded");
     findSportDataDefault();
 });
+
+closePopupBtn.addEventListener('click', closePopup);
 
 
 function findWithKeyword() {
@@ -97,7 +108,8 @@ function waitUntillDataArrvived(){
 function defaultSetNew(index){
     //Elementit
     let eventItem = document.createElement('article');
-    let article = document.createElement('article');
+    //let article = document.createElement('article');
+    article = document.createElement('button');
     let eventNameItem = document.createElement('h2');
     let h2DataTime = document.createElement('h2');
 
@@ -105,23 +117,41 @@ function defaultSetNew(index){
     eventItem.className = "eventItemContainer";
     h2DataTime.className = "eventItem-date";
     article.className = "eventItem-event";
+    article.id = events[index].eventID;
 
     eventNameItem.innerHTML = events[index].eventName;
+    eventNameItem.id = events[index].eventID;
     h2DataTime.innerHTML = events[index].date.toDateString();
 
     //Koostaminen
     eventItem.appendChild(h2DataTime);
     article.appendChild(eventNameItem);
     article.addEventListener('click', (event) => {
-        console.log("Event: " + event.target.innerHTML);
+        console.log("Event ID: " + event.target.getAttribute('id'));
+        let eventID = event.target.getAttribute('id');
+        openPopup(eventID);
+
     });
     eventItem.appendChild(article);
     divElem.appendChild(eventItem);
 }
 
-function openPopup(){
-    let popup
+function openPopup(id){
+    popup.classList.add('open-popup');
+    for (let i = 0; i < newDict.length; i++) {
+        if (newDict[i].eventID.match(id)) {
+            console.log("Löyty " + newDict[i].eventID + " === " + id);
+            popupHeader.innerHTML = newDict[i].eventData.eventName;
+            popupDescription.innerHTML = newDict[i].eventData.eventDescription.intro;
+        } else {
+            console.log("Ei löydy " + newDict[i].eventData.eventName);
+        }
+    }
 }
+function closePopup(){
+    popup.classList.remove('open-popup');
+}
+
 
 
 function eventSetBySearch(index){
@@ -146,49 +176,6 @@ function eventSetBySearch(index){
     divElem.appendChild(eventItem);
 }
 
-
-function defaultEventSet(index){
-    //Indeksimuuttujat
-    let date = currentSearch.resultJson[index].event_dates.starting_day;
-    let eventName = currentSearch.resultJson[index].name.fi;
-
-    //Elementit
-    let eventItem = document.createElement('article');
-    let article = document.createElement('article');
-    let eventNameItem = document.createElement('h2');
-    let h2DataTime = document.createElement('h2');
-
-    //Luokat
-    eventItem.className = "eventItemContainer";
-    h2DataTime.className = "eventItem-date";
-    article.className = "eventItem-event";
-
-
-    date = date.split('T');
-    let datestring = date;
-    let datestring1 = datestring[0].split('-');
-    let dateFixed = swapElements(datestring1);
-    console.log(dateFixed);
-
-
-
-    eventNameItem.innerHTML = eventName;
-    h2DataTime.innerHTML = dateFixed.toString();
-
-    //Koostaminen
-    eventItem.appendChild(h2DataTime);
-    article.appendChild(eventNameItem);
-    eventItem.appendChild(article);
-    divElem.appendChild(eventItem);
-}
-
-function swapElements(array) {
-    let date = array[2].toString() + ".";
-    date += array[1].toString() + ".";
-    date += array[0].toString();
-    return date;
-}
-
 function sortData(data){
     let jsonDate;
     let idOfDate;
@@ -196,6 +183,7 @@ function sortData(data){
     let location;
     let infoUrl;
     let tags;
+    let description;
     console.log("datan määrä: " + data.length);
     let eventDictionary = [];
     for (let i = 0; i < data.length; i++){
@@ -238,88 +226,46 @@ function sortData(data){
                                 tags = 'tags missing!';
                             }
                             finally {
-                                let date = new Date(jsonDate);
-                                eventDictionary.push({eventName:eventName, eventID:idOfDate, date:date, eventLocation: location,eventInfoUrl:infoUrl, eventTags:tags});
+                                try {
+                                    description = data[i].description;
+                                }catch (err){
+                                    console.log("Jokin virhe kuvailussa " + err.stack);
+                                    description = 'description missing';
+                                }finally {
+                                    let date = new Date(jsonDate);
+                                    eventDictionary.push({eventName:eventName, eventID:idOfDate,
+                                                          date:date, eventLocation: location,eventInfoUrl:infoUrl,
+                                                          eventTags:tags, eventDescription: description});
+                                    newDict.push({
+                                        eventID:idOfDate,
+                                        eventData:{
+                                            eventName:eventName,
+                                            date:date,
+                                            eventLocation: location,
+                                            eventInfoUrl:infoUrl,
+                                            eventTags:tags,
+                                            eventDescription: description
+                                        }});
+                                }
                             }
                         }
                     }
-
-                    //eventDictionary[idOfDate] = date.toDateString();
                 }
             }
         }
     }
-    //console.log("Event dict " + eventDictionary.date);
+
     return  sortingDict(eventDictionary);
 }
 
 function sortingDict(dict){
 
-    // let before = dict.slice(0, 5);
-    // for (let i = 0; i < before.length; i++){
-    //     //console.log("before: key " + dict[i].eventID);
-    //     console.log("before: key " + before[i].eventID + " " + " before: value " + before[i].date);
-    // }
-    //
-    // let array = [{eventID: currentSearch.resultJson[0].id, date: currentSearch.resultJson[0].event_dates.starting_day}];
-    // array.push({eventID: currentSearch.resultJson[1].id, date: currentSearch.resultJson[1].event_dates.starting_day});
-    // array.push({eventID: currentSearch.resultJson[2].id, date: currentSearch.resultJson[2].event_dates.starting_day});
-    // for (let i = 0; i < dict.length; i++){
-    //     //console.log("before: key " + dict[i].eventID);
-    //     console.log("before: value " + dict[i].date);
-    // }
-    // console.log("before: key " + dict[0].eventID);
-    // console.log("before: value " + dict[0].date);
-    // console.log("before: " + array[1].date)
-    // console.log("before: " + array[2].date)
-    //
     dict.sort(function(a,b){
         return new Date(a.date) - new Date(b.date);
     });
-    //
-    // let after = dict.slice(0, 5);
-    // for (let i = 0; i < after.length; i++){
-    //     //console.log("after: key " + dict[i].eventID);
-    //     console.log("after: key " + after[i].eventID + " " + " after: value " + after[i].date);
-    // }
-    // // console.log("after: " + array[0].date)
-    // // console.log("after: " + array[1].date)
-    // // console.log("after: " + array[2].date)
-    // // //console.log("Testi arr: id " + array[0].eventID + "Date: " + array.date);
+
     return dict;
 }
-
-
-
-
-
-    // function findSportData() {
-    //     //Luetaan käyttäjänsyöte
-    //     keyword = searchInputField.value;
-    //     //Luodaan hakuolio
-    //     currentSearch = new SearchData();
-    //     //Tehdään haku
-    //     currentSearch.doQuery(apiUrlSearchTab, keyword);
-    //
-    //     setTimeout(function () {
-    //         for (let i = 0; i < currentSearch.resultJson.length; i++) {
-    //             console.log(currentSearch.resultJson[i].name.fi);
-    //             let article =
-    //                 `
-    //             <article>
-    //                 <header class="article">
-    //                     <h2>${currentSearch.resultJson[i].name.fi}</h2>
-    //                 </header>
-    //             </article>
-    //             `
-    //             divElem.innerHTML += article;
-    //         }
-    //     }, 100)
-    // };
-
-//document.addEventListener("DOMContentLoaded", findSportData);
-//searchButton.addEventListener('click', findSportData);
-
 
 
 
