ded0765a20d83698357f05e1a33a943099fa1f31
Kaspar <kaspar.tullus@gmail.com>
2022-12-07 03:44:04 +0200

    Culture.js styled, added loading gif idea?

--- js/culture.js
+++ js/culture.js
@@ -2,11 +2,12 @@
 import SearchData from '../api/myHelsinkiApiNew.js';
 import MapData from "../js/MapApi.js";
 
-const mainElem = document.getElementById("cultureMain");
+const mainElem = document.getElementById("MAINDATA");
 const searchButton = document.getElementById('hakunappi');
 const searchInputField = document.getElementById('hakuteksti');
 const apiUrlSearchTab = "v1/events/?tags_search=";
 
+let loader;
 //popup
 const popup = document.getElementById('popup');
 const closePopupBtn = document.getElementById('closePopup');
@@ -26,8 +27,6 @@ let mapoptiondata =
     };
 
 let currentSearch;
-let waitTime = 0;
-let currentDay;
 let keyword;
 let tagi = 0;
 const datalist = [{}];
@@ -39,8 +38,9 @@ const tags = [{
     name: "Elokuvat,"
 }];
 findCultureData()
+
 function findCultureData() {
-    if (tagi == tags.length) {
+    if (tagi === tags.length) {
         console.log("DONE")
         tagi = 0;
     }
@@ -52,6 +52,7 @@ function findCultureData() {
     currentMAP = new MapData();
     //Tehdään haku
     currentSearch.doQuery(apiUrlSearchTab + tags[tagi].name, keyword);
+    mainElem.innerHTML = `<img class="loader-icon" id="loadIcon" src="../LoadingGifs/CultureLoad.gif" alt="loadingGif">`;
     setTimeout(function () {
         LaunchCultureData();
     }, 1000)
@@ -139,6 +140,7 @@ function LaunchCultureData() {
             }, 2000);
         } else {
             console.log("DONE")
+            mainElem.innerHTML = '';
             SHOWDATA()
         }
 
@@ -146,89 +148,91 @@ function LaunchCultureData() {
     }, 200);
 
 }
-    function SHOWDATA() {
 
-        for (let i = 1; i < datalist.length; i++) {
+function SHOWDATA() {
 
-            if (datalist[i].image === null && datalist[i].description === null || datalist[i].bodydes === null) {
-                console.log("no info,not adding")
-            } else {
+    for (let i = 1; i < datalist.length; i++) {
 
-                let articletwo = document.createElement("article");
-                articletwo.className = "CULTDATARTCLASStwo";
+        if (datalist[i].image === null && datalist[i].description === null || datalist[i].bodydes === null) {
+            console.log("no info,not adding")
+        } else {
 
-                let article = document.createElement("article");
-                article.className = "CULTDATARTCLASS";
-                article.id = "DataARTICLE" + i;
-                article.classList.add('CULTDATARTCLASS', 'new-CULTDATARTCLASS');
+            let articletwo = document.createElement("article");
+            articletwo.className = "CULTDATARTCLASStwo";
+            articletwo.classList.add('CULTDATARTCLASStwo', 'new-CULTDATARTCLASS');
+            let article = document.createElement("div");
+            article.className = "CULTDATARTCLASS";
+            article.id = "DataARTICLE" + i;
 
-                let img = document.createElement("img");
-                img.className = "CultIMG";
+            let img = document.createElement("img");
+            img.className = "CultIMG";
 
-                let IMGSRC;
-                try {
-                    IMGSRC = datalist[i].image;
-                } catch (err) {
-                    console.log("NOPICTURE")
-                    IMGSRC = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png";
-                } finally {
-                    img.src = datalist[i].image;
-                }
-
-                let h3 = document.createElement("h3");
-                h3.innerText = datalist[i].name;
-                h3.className = "DataHEADER";
-                h3.style = " margin: 1%;" +
-                    "padding: 0;" +
-                    "width: 100%;";
-
-                let p = document.createElement("p");
-                p.className = "DataPARAGRAPH";
-                p.innerHTML = datalist[i].bodydes
-                let br = document.createElement("br");
-
-                let button = document.createElement("button");
-                button.id = "Karttanappi" + i;
-
-
-                let mapdiv = document.createElement("div");
-                mapdiv.className = "map";
-                mapdiv.id = "map";
-                mapdiv.style = "width: 95%; height: 300px;";
-
-                mainElem.appendChild(br)
-                mainElem.appendChild(articletwo)
-                articletwo.appendChild(h3);
-                mainElem.appendChild(article);
-                article.appendChild(img);
-                article.appendChild(p);
-                let buttonid = document.getElementById("Karttanappi" + i)
-                try {
-
-                    datalocation = datalist[i].locationlat
-                    datalatcation = datalist[i].locationlon
-                } catch (err) {
-                    console.log("ERRORLOCATION");
-                    datalocation = 'Error';
-                    try {
-                        buttonid.remove();
-                    } catch (err) {
-                        console.log("ERROR REMOVINGBUTTON")
-                    }
-                } finally {
-                        button.innerText = "KARTTA";
-                    button.onclick = function () {
-                        naytamap(i);
-                    };
-                    article.appendChild(button);
-                }
-
+            let IMGSRC;
+            try {
+                IMGSRC = datalist[i].image;
+            } catch (err) {
+                console.log("NOPICTURE")
+                IMGSRC = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png";
+            } finally {
+                img.src = datalist[i].image;
             }
 
+            let articleheader = document.createElement("div");
+            articleheader.innerText = datalist[i].name;
+            articleheader.className = "DataARTICLE";
+            articleheader.style = " margin: 1%;" +
+                "padding: 0;" +
+                "width: 100%;";
+
+            let p = document.createElement("p");
+            p.className = "DataPARAGRAPH";
+            p.innerHTML = datalist[i].bodydes
+            let br = document.createElement("br");
+
+            let button = document.createElement("button");
+            button.className = "Karttanappi" + i
+            button.id = "Karttanappi";
+
+
+            let mapdiv = document.createElement("div");
+            mapdiv.className = "map";
+            mapdiv.id = "map";
+            mapdiv.style = "width: 95%; height: 300px;";
+
+            mainElem.appendChild(br)
+            mainElem.appendChild(articletwo)
+            articletwo.appendChild(articleheader);
+            articleheader.appendChild(img);
+            articletwo.appendChild(article);
+            article.appendChild(p);
+
+            let buttonid = document.getElementById("Karttanappi" + i)
+            try {
+
+                datalocation = datalist[i].locationlat
+                datalatcation = datalist[i].locationlon
+            } catch (err) {
+                console.log("ERRORLOCATION");
+                datalocation = 'Error';
+                try {
+                    buttonid.remove();
+                } catch (err) {
+                    console.log("ERROR REMOVINGBUTTON")
+                }
+            } finally {
+                button.innerText = "KARTTA";
+                button.onclick = function () {
+                    naytamap(i);
+                };
+                article.appendChild(button);
+            }
 
         }
 
+
     }
+
+}
 
 //NÄYTETÄÄN KARTTA (DATALINDEX) = napissa oleva index arvo mikä tehtiin forloopissa ylhäällä.
 function naytamap(datalindex) {
@@ -236,10 +240,10 @@ function naytamap(datalindex) {
     console.log("Löyty " + datalist[datalindex].id + " === " + datalindex);
     popupHeader.innerHTML = datalist[datalindex].name;
     popupDescription.innerHTML = datalist[datalindex].description;
-    if (counter === 1){
-        try{
+    if (counter === 1) {
+        try {
             currentMAP.mapleaf.remove()
-        }catch (err){
+        } catch (err) {
             console.log("ERRORREMVOVE")
         }
 
@@ -279,29 +283,28 @@ function naytamap(datalindex) {
                         console.log(NAME)
                         try {
                             currentMAP.Lmarker = L.marker([currentMAP.posLat, currentMAP.posLong]).addTo(currentMAP.mapleaf).bindPopup(NAME);
-                        }catch (err){
+                        } catch (err) {
                             console.log("ERROR MARKER")
-                        }finally {
+                        } finally {
                             counter = 1;
                             console.log("GOTLOCATION")
                             currentMAP.showMap();
                         }
 
-                        }
                     }
-
                 }
+
             }
         }
+    }
 
 
 }
 
-function closePopup(){
+function closePopup() {
     popup.classList.remove('open-popup');
     currentMap.mapleaf.remove('map');
 }
-
 
 
 closePopupBtn.addEventListener('click', closePopup);
