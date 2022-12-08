d59bb10668715a36f9d3494272b93732bba12872
Semen Morozov <semen.morozov@edu.omnia.fi>
2022-12-08 09:49:46 +0200

    movie osa tulee

---
+++ api/FinnkinoData.js
@@ -0,0 +1,49 @@
+export default class SearchDataFINNKINO {
+
+    constructor() {
+        this.Filmkuvalista = [{}];
+    }
+
+    loadDoc() {
+        let dropdown = document.getElementById("custom-select");
+        let filmsList = this.Filmkuvalista;
+        fetch('https://www.finnkino.fi/xml/Schedule/')
+            .then((res) => res.text())
+            .catch((error) => {
+                console.error('Error:', error);
+            })
+            .then(function (data) {
+                let parser = (new window.DOMParser()).parseFromString(data, 'text/xml');
+
+                for (let i = 0; i < parser.getElementsByTagName('Show').length; i++) {
+
+                    let title = parser.getElementsByTagName('Title')[i].childNodes[0].nodeValue;
+                    let genres = parser.getElementsByTagName('Genres')[i].childNodes[0].nodeValue;
+                    let filmImagelarge = parser.getElementsByTagName('EventLargeImagePortrait')[i].childNodes[0].nodeValue;
+                    let rating = parser.getElementsByTagName('Rating')[i].childNodes[0].nodeValue;
+                    let filmImagesmall = parser.getElementsByTagName('EventSmallImagePortrait')[i].childNodes[0].nodeValue;
+                    let movietime = parser.getElementsByTagName('LengthInMinutes')[i].childNodes[0].nodeValue;
+                    let year = parser.getElementsByTagName('ProductionYear')[i].childNodes[0].nodeValue;
+                    let showurl = parser.getElementsByTagName('ShowURL')[i].childNodes[0].nodeValue;
+                    let theatreName = parser.getElementsByTagName('Theatre')[i].childNodes[0].nodeValue;
+                    let theatreID = parser.getElementsByTagName('TheatreID')[i].childNodes[0].nodeValue;
+
+                    filmsList.push({
+                        srcL: filmImagelarge,
+                        srcS: filmImagesmall,
+                        name: title,
+                        movieGenre: genres,
+                        movieRating: rating,
+                        movieduration: movietime,
+                        movieyear: year,
+                        movieurl: showurl,
+                        cinemaName: theatreName,
+                        cinemaID: theatreID
+                    });
+                }
+
+            });
+        this.Filmkuvalista = filmsList;
+
+    }
+}
